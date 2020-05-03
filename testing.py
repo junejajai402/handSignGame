46a14ed1eafcb60defdeb4ce744132c083b37d4e
junejajai402 <64757026+junejajai402@users.noreply.github.com>
2020-05-03 19:34:09 -0400

    initial upload
    First attempt in a while

---
+++ testing.py
@@ -0,0 +1,37 @@
+import random
+
+container=[1,2,3]
+resolvers=['Rock','Paper','Scissors']
+#print(random.choice(container))
+val=0
+score=[0,0]
+while True:
+    g = input("Enter 1.Rock 2.Paper 3.Scissor: ")
+    g.lower()
+    if g=='rock':
+        val=1
+    elif g=='paper':
+        val=2
+    else:
+        val=3
+
+    compgen_Val=random.choice(container)
+    if val==3 and compgen_Val==1:
+        score[1]=score[1]+1
+        print("You lose this round. Rock beats Scissors")
+    elif val==1 and compgen_Val==3:
+        score[0]=score[0]+1
+        print("You win this round. Rock beats Scissors")
+    elif compgen_Val>val:
+        score[1]=score[1]+1
+        print("You lose this round. %s beats %s" %(resolvers[compgen_Val-1],resolvers[val-1]))
+    elif val>compgen_Val:
+        score[0]=score[0]+1
+        print("You win this round. %s beats %s" %(resolvers[val-1] ,resolvers[compgen_Val-1]))
+    else:
+        print("you both played the same hand sign. Its a draw")
+        continue
+
+    if score[0]==5 or score[1]==5:
+        break
+print("the final score is computer: %2d player: %2d" %(score[1],score[0]))
